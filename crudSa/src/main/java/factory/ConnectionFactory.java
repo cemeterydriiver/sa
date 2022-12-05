6717274e0460d8597b34d43d03b572b41fab5a64
cemeterydriiver <anaclaradsouza2005@gmail.com>
2022-12-04 22:35:37 -0300

    .

---
+++ crudSa/src/main/java/factory/ConnectionFactory.java
@@ -0,0 +1,21 @@
+
+package factory;
+
+
+import java.sql.Connection;
+
+import java.sql.DriverManager;
+
+import java.sql.SQLException;
+
+
+public class ConnectionFactory {
+
+    public Connection getConnection() {
+        try {
+            return DriverManager.getConnection("jdbc:mysql://localhost/db_usuario", "root", "");
+        } catch (SQLException excecao) {
+            throw new RuntimeException(excecao);
+        }
+    }
+}
